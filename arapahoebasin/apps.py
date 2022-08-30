1114cfb7249d453ddf03c7fd5e84b44beb994e3b
ampersand-paris <ampersand.s.paris@gmail.com>
2022-08-30 11:37:52 -0400

    django startproject and startapp

--- arapahoebasin/apps.py
+++ arapahoebasin/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AraphoebasinConfig(AppConfig):
+class ArapahoebasinConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'araphoebasin'
+    name = 'arapahoebasin'
